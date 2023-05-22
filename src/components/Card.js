071519ee19c5635a2a6895fd491c658559327304
Sidney Sudatti <s02devmail@protonmail.com>
2023-05-22 01:42:11 -0300

    refactor: fix open Popup function

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,7 @@
 export default function Card(props) {
+  function handleClick() {
+    props.onCardClick(props.card);
+  }
   return (
     <li className="picture-card">
       <button className="picture-card__delete-btn picture-card__delete-btn_hidden"></button>
@@ -6,9 +9,7 @@ export default function Card(props) {
         className="picture-card__image"
         alt="card"
         src={props.card.link}
-        onClick={() => {
-          props.onCardClick(props.card);
-        }}
+        onClick={handleClick}
       />
       <div className="card-wrap">
         <h2 className="picture-card__description">{props.card.name}</h2>
