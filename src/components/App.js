1d3376f473bd989624fa85d6ada5a5984a4fa6aa
Sidney Sudatti <s02devmail@protonmail.com>
2023-05-22 00:13:17 -0300

    Refactor: files organization

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import "./index.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import "../index.css";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 import { useState } from "react";
-import PopupWithForm from "./components/PopupWithForm";
-import ImagePopup from "./components/ImagePopup";
+import PopupWithForm from "./PopupWithForm";
+import ImagePopup from "./ImagePopup";
 
 function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
