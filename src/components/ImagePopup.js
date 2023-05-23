2dff20a56237a407d300e2f4ec6c3bc6f0618721
Sidney Sudatti <s02devmail@protonmail.com>
2023-05-22 22:33:14 -0300

    Fix: fix the pop-ups position

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,23 +1,25 @@
 export default function ImagePopup(props) {
   return (
-    <div
-      className={`popup-container ${
-        props.card ? "overlay" : "overlay-disable"
-      }`}
-    >
-      <div className="popup-element">
-        <button
-          className="modal__close-btn popup__close-btn"
-          onClick={props.onClose}
-        ></button>
-        <img
-          className="popup__image"
-          src={props.card.link}
-          alt={`${props.card.name} image`}
-        />
-        <h2 className="picture-card__description popup__title">
-          {props.card.name}
-        </h2>
+    <div className="pictures-container">
+      <div
+        className={`popup-container ${
+          props.card ? "overlay" : "overlay-disable"
+        }`}
+      >
+        <div className="popup-element">
+          <button
+            className="modal__close-btn popup__close-btn"
+            onClick={props.onClose}
+          ></button>
+          <img
+            className="popup__image"
+            src={props.card.link}
+            alt={`${props.card.name} image`}
+          />
+          <h2 className="picture-card__description popup__title">
+            {props.card.name}
+          </h2>
+        </div>
       </div>
     </div>
   );
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,33 +5,35 @@ function PopupWithForm(props) {
     props;
 
   return (
-    <div
-      className={`general-modal ${modalType} ${
-        isOpen ? "overlay" : "overlay-disable"
-      }`}
-    >
-      <div className="modal">
-        <form
-          action="#"
-          className={`modal-container form ${formType}`}
-          noValidate
-        >
-          <button
-            className="modal__close-btn"
-            type="button"
-            onClick={onClose}
-          ></button>
-          <h2 className="modal__title">{title}</h2>
-          {children}
-          <div className="modal__container">
+    <div className="pictures-container">
+      <div
+        className={`general-modal ${modalType} ${
+          isOpen ? "overlay" : "overlay-disable"
+        }`}
+      >
+        <div className="modal">
+          <form
+            action="#"
+            className={`modal-container form ${formType}`}
+            noValidate
+          >
             <button
-              className="modal__submit-btn modal__submit-btn_error"
-              type="submit"
-            >
-              {buttonText}
-            </button>
-          </div>
-        </form>
+              className="modal__close-btn"
+              type="button"
+              onClick={onClose}
+            ></button>
+            <h2 className="modal__title">{title}</h2>
+            {children}
+            <div className="modal__container">
+              <button
+                className="modal__submit-btn modal__submit-btn_error"
+                type="submit"
+              >
+                {buttonText}
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
